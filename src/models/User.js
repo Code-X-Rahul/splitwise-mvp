213f76ea2d36ac0fd86dcd4bcd38ffd9294a2042
Rahul Rajput <evileyeparadise@gmail.com>
2026-05-09 09:32:03 +0530

    feat: added exoense and expense member relation to user

--- src/models/User.js
+++ src/models/User.js
@@ -2,52 +2,48 @@ import Sequelize, { Model } from "sequelize";
 import bcrypt from "bcryptjs";
 
 class User extends Model {
-  static init(sequelize) {
-    super.init(
-      {
-        name: Sequelize.STRING,
-        email: Sequelize.STRING,
-        password: Sequelize.VIRTUAL, //When it is VIRTUAL it does not exist in the database
-        password_hash: Sequelize.STRING,
-        currency: {
-          type: Sequelize.STRING,
-          defaultValue: "INR",
-        },
-      },
-      {
-        sequelize,
-        timestamps: true, //If it's false do not add the attributes (updatedAt, createdAt).
-        //paranoid: true, //If it's true, it does not allow deleting from the bank, but inserts column deletedAt. Timestamps need be true.
-        //underscored: true, //If it's true, does not add camelcase for automatically generated attributes, so if we define updatedAt it will be created as updated_at.
-        //freezeTableName: false, //If it's false, it will use the table name in the plural. Ex: Users
-        //tableName: 'Users' //Define table name
-      },
-    );
+	static init(sequelize) {
+		super.init(
+			{
+				name: Sequelize.STRING,
+				email: Sequelize.STRING,
+				password: Sequelize.VIRTUAL, //When it is VIRTUAL it does not exist in the database
+				password_hash: Sequelize.STRING,
+				currency: {
+					type: Sequelize.STRING,
+					defaultValue: "INR",
+				},
+			},
+			{
+				sequelize,
+				timestamps: true,
+			},
+		);
 
-    this.addHook("beforeSave", async (user) => {
-      if (user.password) {
-        user.password_hash = await bcrypt.hash(user.password, 8);
-      }
-    });
+		this.addHook("beforeSave", async (user) => {
+			if (user.password) {
+				user.password_hash = await bcrypt.hash(user.password, 8);
+			}
+		});
 
-    return this;
-  }
+		return this;
+	}
 
-  static associate(models) {
-    this.hasMany(models.Expense, {
-      foreignKey: "createdBy",
-      as: "createdExpenses",
-    });
-    this.belongsToMany(models.Expense, {
-      through: "ExpenseMembers",
-      foreignKey: "userId",
-      as: "expenses",
-    });
-  }
+	static associate(models) {
+		this.hasMany(models.Expense, {
+			foreignKey: "createdBy",
+			as: "createdExpenses",
+		});
+		this.belongsToMany(models.Expense, {
+			through: "ExpenseMembers",
+			foreignKey: "userId",
+			as: "expenses",
+		});
+	}
 
-  checkPassword(password) {
-    return bcrypt.compare(password, this.password_hash);
-  }
+	checkPassword(password) {
+		return bcrypt.compare(password, this.password_hash);
+	}
 }
 
 export default User;
