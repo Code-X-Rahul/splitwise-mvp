7a4a5e74cc0f1f7e0cab5fa4c62fb469e7d4de8b
Rahul Rajput <evileyeparadise@gmail.com>
2026-05-09 10:03:17 +0530

    refactor: removed aws service

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import dotenv from "dotenv";
 import expressService from "./services/express.service";
 import sequelizeService from "./services/sequelize.service";
-import awsService from "./services/aws.service";
 dotenv.config();
 
-const services = [expressService, awsService, sequelizeService];
+const services = [expressService, sequelizeService];
 
 (async () => {
-  try {
-    for (const service of services) {
-      await service.init();
-    }
-    console.log("Server initialized.");
-    //PUT ADITIONAL CODE HERE.
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	try {
+		for (const service of services) {
+			await service.init();
+		}
+		console.log("Server initialized.");
+		//PUT ADITIONAL CODE HERE.
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 })();
